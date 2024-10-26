08722602aab5bed5ad5d176271f3ead6cf7adc32
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-10-26 20:04:22 +0100

    scroll to top btn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 // Placehilder-wave working just during the img or iframe is downloading
-
 const placeholders = document.querySelectorAll(".placeholder-wave");
 
 placeholders.forEach((element) => {
@@ -11,3 +10,25 @@ placeholders.forEach((element) => {
     element.onload();
   }
 });
+
+// Scroll button
+window.onscroll = function () {
+  toggleScrollButton();
+};
+
+function toggleScrollButton() {
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+  if (document.documentElement.scrollTop > 300) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+}
+
+// Go to top on click
+document.getElementById("scrollTopBtn").onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
