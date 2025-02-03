dee1130445ba879988a9b51fa7b352018bc330ca
Anna Rychkova <aanytkaa@gmail.com>
2025-02-03 15:43:00 +0000

    notification

--- js/index.js
+++ js/index.js
@@ -32,3 +32,37 @@ document.getElementById("scrollTopBtn").onclick = function () {
     behavior: "smooth",
   });
 };
+
+// notification on the top
+document.addEventListener("DOMContentLoaded", function () {
+  // create HTML for notification
+  const notificationHTML = `
+    <aside class="notification">
+      <h2>Attention!
+        <svg class="close-icon" aria-label="Info">
+          <use href="images/symbol-defs.svg#info"></use>
+        </svg>
+      </h2>
+      <p>
+        This website has no affiliation with the mentioned online store. We are not responsible for the products, prices, or services provided through third-party websites.
+      </p>
+      <p>
+        Please contact the store directly for any assistance or support. We are not official representatives of the store.
+      </p>
+      <p>
+        If you have any questions, feel free to reach out to us via <a href="mailto:aanytkaa@gmail.com">email</a>.
+      </p>
+      <button class="close-btn" onclick="this.parentElement.style.display='none';" aria-label="Close">
+        <svg class="close-icon" aria-label="Close">
+          <use href="images/symbol-defs.svg#close"></use>
+        </svg>
+      </button>
+    </aside>
+  `;
+
+  // get the container for the notification
+  const container = document.getElementById("notification-container");
+
+  // add notification to the container
+  container.innerHTML = notificationHTML;
+});
